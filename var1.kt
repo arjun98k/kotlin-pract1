4ba4b89ce67beb9127dc998d000f38f746d46254
Akshay9322 <akshaysalekar199710@gmail.com>
2025-01-05 23:49:59 +0530

    list question

--- var1.kt
+++ var1.kt
@@ -3,5 +3,7 @@ fun main(args: Array<String>) {
    val list = listOf(11, 12, 13, 14) // Correct way to define an immutable list
    for (i in list) { // Iterate through the list
        println(i) // Print each element
+       print("shubh ratri")
    }
 }
+ 
