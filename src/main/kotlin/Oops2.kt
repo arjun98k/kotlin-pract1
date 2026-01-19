7b5178c8adda29ce547eb063d5518836405da7b3
arjun98k <arjunkandekar14@gmail.com>
2026-01-19 19:37:44 +0530

    encapsulation

--- src/main/kotlin/Oops2.kt
+++ src/main/kotlin/Oops2.kt
@@ -1,10 +1,34 @@
 fun main() {
-    val person = Person("akash")
-    println(person)
-    println(person)
+//    val person = Person("")
+//  person.setName("arjun")
+//    println(person.getName())
+
+    val person = Person2(0)
+    person.age = 22
+    println(person.age)
+
 }
 
-class Person(name:String) {
+class Person(private var name:String) {
+    fun getName(): String{
+        return name
+    }
 
+    fun setName(n: String){
+        name = n
+    }
+
+}
+
+class Person2( age:Int){
+    private var _age = age
+
+    var age: Int
+        get() = _age
+        set(value) {
+            if (value > 0){
+                _age = value
+            }
+        }
 
 }
