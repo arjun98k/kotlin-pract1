93f6f32f06c48051316d2d3133fac92bb3e0f308
arjun98k <arjunkandekar14@gmail.com>
2026-01-21 12:49:21 +0530

    inheritance

--- src/main/kotlin/Oops4.kt
+++ src/main/kotlin/Oops4.kt
@@ -1,29 +1,29 @@
-fun main() {
-    val dog = Dogs()
-    dog.walk()
-    dog.makeSound()
-    val calculator = calculator()
-    calculator.add(12,13)
-    calculator.add(22,23,45)
-}
-
-open class Animal {
-    fun walk(){
-        println("animal can walk ")
-    }
-    open fun makeSound(){
-        println("animal make sound")
-    }
-
-}
-
-class  Dogs:Animal() {
-    override fun makeSound(){
-        println("dogs make barking sound")
-    }
-}
-
-class calculator {
-    fun add(a:Int,b:Int): Int = a+b
-    fun add(a:Int,b:Int,c:Int): Int = a+b+c
-}
+//fun main() {
+//    val dog = Dogs()
+//    dog.walk()
+//    dog.makeSound()
+//    val calculator = calculator()
+//    calculator.add(12,13)
+//    calculator.add(22,23,45)
+//}
+//
+//open class Animal {
+//    fun walk(){
+//        println("animal can walk ")
+//    }
+//    open fun makeSound(){
+//        println("animal make sound")
+//    }
+//
+//}
+//
+//class  Dogs:Animal() {
+//    override fun makeSound(){
+//        println("dogs make barking sound")
+//    }
+//}
+//
+//class calculator {
+//    fun add(a:Int,b:Int): Int = a+b
+//    fun add(a:Int,b:Int,c:Int): Int = a+b+c
+//}
